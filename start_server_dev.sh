224bd1910c82cdc3ba52c5999fbc86945041432f
Mayfly277 <mayfly277@gmail.com>
2021-01-19 23:59:59 +0100

    css

--- start_server_dev.sh
+++ start_server_dev.sh
@@ -9,5 +9,6 @@ fi ;
 cd $DIR
 DIR=$(realpath $DIR)
 sudo docker build -t rec_browse:latest .
-sudo docker run --name "rec_browse" --rm -d -p "127.0.0.1:5000:5000" -v "$DIR/casts:/app/static/casts" rec_browse:latest
+sudo docker stop rec_browse
+sudo docker run --name "rec_browse" --rm -d -p "127.0.0.1:5000:5000" -v "$DIR/casts:/app/static/casts" -v "$DIR/webapp:/app" rec_browse:latest
 echo "Web ihm at : http://127.0.0.1:5000/"
