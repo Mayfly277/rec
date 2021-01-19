c5e8e34841dd7b03cf37855046c75de86fee79f1
Mayfly277 <mayfly277@gmail.com>
2021-01-19 23:00:34 +0100

    start detached

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR
 sudo docker build -t rec_browse:latest .
-sudo docker run --name "rec_browse" --rm -p "127.0.0.1:5000:5000" -v "$DIR/casts:/app/static/casts" rec_browse:latest
+sudo docker run --name "rec_browse" --rm -d -p "127.0.0.1:5000:5000" -v "$DIR/casts:/app/static/casts" rec_browse:latest
 echo "Web ihm at : http://127.0.0.1:5000/"
