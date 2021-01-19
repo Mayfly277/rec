263d27f2018cad909cb15780b905f890fb007034
Mayfly277 <mayfly277@gmail.com>
2021-01-20 00:11:32 +0100

    fix and homepage

--- webapp/server.py
+++ webapp/server.py
@@ -3,21 +3,23 @@
 from flask import Flask, render_template
 import os
 import glob
+import collections
 
 app = Flask(__name__, template_folder='.')
 
 @app.route("/")
 def home():
     file_list = glob.glob("static/casts/*.cast")
-    links=[]
+    links={}
     for cast_file in file_list:
         target = cast_file
         info_file = open(cast_file[:-4] + 'info', 'r')
         infos=info_file.read()
         [date,cmd] = infos.split(' : ')
         info_file.close()
-        links.append({'target' : target, 'date': date, 'title'  : cmd.replace("\n","")})
-    return render_template("default.html", title='Home', links=links)
+        links[date]={'target' : target, 'title'  : cmd.replace("\n","")}
+    od = collections.OrderedDict(sorted(links.items()))
+    return render_template("default.html", title='Home', links=od)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
