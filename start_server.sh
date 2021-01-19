d7c5a144427c0878fdb1205f635142d5dbae3973
Mayfly277 <mayfly277@gmail.com>
2021-01-19 23:06:48 +0100

    fix start_server path

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+if [ -L $0 ] ; then
+    DIR=$(dirname $(readlink -f $0)) ;
+else
+    DIR=$(dirname $0) ;
+fi ;
+
 cd $DIR
 sudo docker build -t rec_browse:latest .
 sudo docker run --name "rec_browse" --rm -d -p "127.0.0.1:5000:5000" -v "$DIR/casts:/app/static/casts" rec_browse:latest
